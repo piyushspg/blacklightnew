d712aa80f307d1158b7531415847a07b85e17a8f
piyushspg <83351871+piyushspg@users.noreply.github.com>
2024-03-10 17:05:58 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 
 const pool = mysql.createPool({
-    host: "localhost", // Replace with your database host
-    user: "root", // Replace with your database username
-    password: "", // Replace with your database password
-    database: "leadenhall", // Replace with your database name
+    host: "sql6.freesqldatabase.com", // Replace with your database host
+    user: "sql6690105", // Replace with your database username
+    password: "fqbQ52qKNt", // Replace with your database password
+    database: "sql6690105", // Replace with your database name
   });
 
 // Export the pool for reuse
